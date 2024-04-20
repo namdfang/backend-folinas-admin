fe3050fffa64ecf0eea11319d98ed7c4c5b446da
namdang <namdang06092001@gmail.com>
2024-04-20 07:34:33 +0700

    Initial project

---
+++ src/api/banner-top/routes/banner-top.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * banner-top router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::banner-top.banner-top');
